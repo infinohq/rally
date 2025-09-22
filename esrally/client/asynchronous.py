cde13016fb40bb317131338fdf5b550db985288b
Ekechi Nwokah <ekechi.nwokah@infino.ai>
2025-09-22 18:13:20 -0400

    fix syntax error

--- esrally/client/asynchronous.py
+++ esrally/client/asynchronous.py
@@ -678,9 +678,8 @@ class RallyAsyncDatabase(AsyncElasticsearch, RequestContextHolder):
         # HEAD with a 404 is returned as a normal response
         # since this is used as an 'exists' functionality.
         if not (method == "HEAD" and meta.status == 404) and (
-{{ ... }}
-            and (self._ignore_status is DEFAULT or self._ignore_status is None or meta.status not in self._ignore_status)
-        ):
+            meta.status < 200 or meta.status >= 300
+        ) and (self._ignore_status is DEFAULT or self._ignore_status is None or meta.status not in self._ignore_status):
             message = str(resp_body)
 
             # If the response is an error response try parsing
